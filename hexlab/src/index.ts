eae137795ccb8f2274a1a60e91500e71387f58c5
Eric Gentry <ericsnekbytes@gmail.com>
2023-08-06 17:47:19 -0400

    Basic compatibility.

--- hexlab/src/index.ts
+++ hexlab/src/index.ts
@@ -1,8 +1,13 @@
 import {
+  ILabShell,
   ILayoutRestorer,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+
+import {
+  INotebookShell
+} from '@jupyter-notebook/application';
 
 import {
   ICommandPalette,
@@ -731,7 +736,58 @@ class HexEditorWidget extends Widget {
 /**
 * Activate the hexlab widget extension.
 */
-function activate(app: JupyterFrontEnd, palette: ICommandPalette, restorer: ILayoutRestorer | null) {
+function activateLab(app: JupyterFrontEnd, palette: ICommandPalette, _shell: ILabShell, restorer: ILayoutRestorer | null) {
+  console.log('[Hexlab] JupyterLab extension hexlab is activated!');
+
+  // Declare a widget variable
+  let widget: Panel;
+
+  // Add an application command
+  const command: string = 'hexlab:open';
+  app.commands.addCommand(command, {
+    label: 'Hex Editor',
+    execute: () => {
+      if (!widget || widget.isDisposed) {
+        const content = new HexEditorWidget();
+        widget = new Panel();
+        widget.addWidget(content);
+        widget.id = 'hexlab';
+        widget.title.label = 'Hex Editor';
+        widget.title.closable = true;
+      }
+      if (!tracker.has(widget)) {
+        // Track the state of the widget for later restoration
+        tracker.add(widget);
+      }
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.add(widget, 'main');
+      }
+
+      // Activate the widget
+      app.shell.activateById(widget.id);
+    }
+  });
+
+  // Add the command to the palette.
+  palette.addItem({ command, category: 'Tutorial' });
+
+  // Track and restore the widget state
+  let tracker = new WidgetTracker<Panel>({
+    namespace: 'hexlab'
+  });
+  if (restorer) {
+    restorer.restore(tracker, {
+      command,
+      name: () => 'hexlab'
+    });
+  }
+}
+
+/**
+* Activate the hexlab widget extension.
+*/
+function activateNotebook(app: JupyterFrontEnd, palette: ICommandPalette, _shell: INotebookShell, restorer: ILayoutRestorer | null) {
   console.log('[Hexlab] JupyterLab extension hexlab is activated!');
 
   // Declare a widget variable
@@ -782,12 +838,22 @@ function activate(app: JupyterFrontEnd, palette: ICommandPalette, restorer: ILay
 /**
  * Initialization data for the hexlab extension.
  */
-const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'hexlab:plugin',
+const pluginLab: JupyterFrontEndPlugin<void> = {
+  id: 'hexlab:pluginLab',
   autoStart: true,
-  requires: [ICommandPalette],
+  requires: [ICommandPalette, ILabShell],
   optional: [ILayoutRestorer],
-  activate: activate,
+  activate: activateLab,
 };
 
-export default plugin;
+const pluginNotebook: JupyterFrontEndPlugin<void> = {
+  id: 'hexlab:pluginNotebook',
+  autoStart: true,
+  requires: [ICommandPalette, INotebookShell],
+  optional: [ILayoutRestorer],
+  activate: activateNotebook,
+};
+
+const plugins: JupyterFrontEndPlugin<void>[] = [pluginLab, pluginNotebook];
+
+export default plugins;
